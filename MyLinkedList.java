6b58241cbadc36bcbc013b87d332edec6825c20f
isulley77 <43052223+isulley77@users.noreply.github.com>
2018-12-16 15:53:13 -0500

    Constructor

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,11 +2,21 @@ class MyLinkedList{
   private int size;
   private Node start, end;
 
-  public int size();
-  public boolean add(int value);
-  public String toString();
-
   public MyLinkedList(){
-    
+    size = 0;
+    start = null;
+    end = null;
   }
+
+  public int size(){
+    return size;
+  }
+
+/*
+  public boolean add(Integer value){
+
+     Node n = new Node(value, end.previous, null);
+  }
+  */
+
 }
